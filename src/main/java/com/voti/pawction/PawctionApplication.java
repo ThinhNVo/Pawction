0c545bfab80cab5f1919d49a85f62c0ad8cc5026
ThinhNVo <voti@mail.uc.edu>
2025-10-02 09:47:54 -0400

    initial commit

---
+++ src/main/java/com/voti/pawction/PawctionApplication.java
@@ -0,0 +1,13 @@
+package com.voti.pawction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawctionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PawctionApplication.class, args);
+    }
+
+}

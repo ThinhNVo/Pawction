299c41daec6c9cecf805ee4b988726aae291140e
ThinhNVo <voti@mail.uc.edu>
2025-10-02 15:22:06 -0400

    testcommit

---
+++ src/main/java/com/voti/pawction/PawctionController.java
@@ -0,0 +1,19 @@
+package com.voti.pawction;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PawctionController {
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @RequestMapping("/")
+    public String index() {
+        System.out.println("Application Name: " + appName);
+        return "index";
+    }
+
+}

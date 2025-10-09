897f67b026738b484689202278aeb5d7baead253
ThinhNVo <voti@mail.uc.edu>
2025-10-09 14:46:28 -0400

    full program set up

---
+++ target/classes/db/migration/V5__CreateTransaction.sql
@@ -0,0 +1,12 @@
+create table transaction
+(
+    tx_id       BIGINT auto_increment
+        primary key,
+    account_id  BIGINT                                       not null,
+    tx_type     ENUM ('DEPOSIT', 'WITHDRAWAL', 'SETTLEMENT') not null,
+    amount      decimal(19, 4)                               not null,
+    occurred_at DATETIME default CURRENT_TIMESTAMP           not null,
+    constraint transaction_account_account_id_fk
+        foreign key (account_id) references account (account_id)
+);
+

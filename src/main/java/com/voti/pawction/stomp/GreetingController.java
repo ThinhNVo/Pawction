e25db81115d04374f050263c7f531fe6a4c0fe6d
dinesh2003 <adhikaridinesh76@gmail.com>
2025-10-30 20:53:45 -0400

    created gitignore

---
+++ src/main/java/com/voti/pawction/stomp/GreetingController.java
@@ -0,0 +1,14 @@
+package com.voti.pawction.stomp;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class GreetingController {
+    @MessageMapping("/hello")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+}

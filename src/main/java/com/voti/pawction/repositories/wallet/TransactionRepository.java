7bf5f62e4c1adc748824540a834a547c08867257
ThinhNVo <voti@mail.uc.edu>
2025-11-03 11:40:45 -0500

    add new method to TransactionRepository to return list of latest transaction and custom query to return actual balance after holds

--- src/main/java/com/voti/pawction/repositories/wallet/TransactionRepository.java
+++ src/main/java/com/voti/pawction/repositories/wallet/TransactionRepository.java
@@ -3,5 +3,9 @@ package com.voti.pawction.repositories.wallet;
 import com.voti.pawction.entities.wallet.Transaction;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface TransactionRepository extends CrudRepository<Transaction, Long> {
+
+    List<Transaction> findByAccountAccountIdOrderByCreatedAtDesc(Long accountId);
 }

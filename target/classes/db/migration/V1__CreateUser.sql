c547dd69a77282f8cbfd33a65d392a4180fef8b0
dinesh2003 <adhikaridinesh76@gmail.com>
2025-11-04 14:58:01 -0500

    added pom which was removed

--- target/classes/db/migration/V1__CreateUser.sql
+++
@@ -1,9 +0,0 @@
-create table user
-(
-    user_id       bigint auto_increment
-        primary key,
-    name          varchar(120) not null,
-    email         varchar(255) not null,
-    password_hash varchar(255) not null
-);
-

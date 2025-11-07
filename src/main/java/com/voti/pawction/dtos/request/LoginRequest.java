2e8781b19d22f793e5a119244632a47b2622d6bc
Khagendra Dhungel <dhungeka@mail.uc.edu>
2025-11-07 03:05:45 -0500

    login controller

--- src/main/java/com/voti/pawction/dtos/request/LoginRequest.java
+++ src/main/java/com/voti/pawction/dtos/request/LoginRequest.java
@@ -1,8 +1,14 @@
 package com.voti.pawction.dtos.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequest {
     private String email;
     private String password;

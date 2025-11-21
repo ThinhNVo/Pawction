18f258aa1438fc6e336820add9192a7582349c41
Khagendra Dhungel <dhungeka@mail.uc.edu>
2025-11-21 18:31:21 -0500

    Updating petservice, added ownership

--- src/main/java/com/voti/pawction/controllers/UIController.java
+++ src/main/java/com/voti/pawction/controllers/UIController.java
@@ -1,5 +1,6 @@
 package com.voti.pawction.controllers;
 
+import com.voti.pawction.dtos.request.AuctionRequest.CreateAuctionRequest;
 import com.voti.pawction.dtos.request.UserRequest.LoginRequest;
 import com.voti.pawction.dtos.response.UserDto;
 import com.voti.pawction.mappers.UserMapper;
@@ -10,6 +11,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -44,4 +47,33 @@ public class UIController {
         model.addAttribute("user", user);
         return "index";
     }
+
+
+    @GetMapping ("/auction/add")
+    public String showAddAuctionPage(HttpSession session, Model model, RedirectAttributes redirectAttributes) {
+        if (!isLoggedIn(session)) {
+            redirectAttributes.addFlashAttribute("errorMessage", "You must be logged in to add an auction.");
+            return "redirect:/login";
+        }
+
+        UserDto user = (UserDto) session.getAttribute("loggedInUser");
+
+        model.addAttribute("loggedIn", true);
+        model.addAttribute("user", user);
+        return "add-auction";
+    }
+
+    @PostMapping("/auction/add")
+    public String addAuction(@ModelAttribute CreateAuctionRequest auctionRequest,
+                             HttpSession session,
+                             RedirectAttributes redirectAttributes) {
+        try {
+            auctionService.createAuction(auctionRequest);
+            redirectAttributes.addFlashAttribute("successMessage", "Auction created successfully!");
+            return "redirect:/home";
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("errorMessage", "Failed to create auction: " + e.getMessage());
+            return "redirect:/auction/add";
+        }
+
 }
